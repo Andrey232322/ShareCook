4c2aa48dadb4ec6370c659c9e2a0f36cfa4b5952
Andrey <dwigatel3532@gmail.com>
2024-09-17 17:46:25 +0300

    fff

--- backend/recipes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Tag, Ingredient, Recipe, ShoppingList, RecipeIngredient, Favorite
-# Register your models here.
-admin.site.register(Tag)
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
-admin.site.register(ShoppingList)
-admin.site.register(RecipeIngredient)
-admin.site.register(Favorite)
