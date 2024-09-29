7967f09da22982e68c9c8823fa4bbb517e66a168
Your Name <you@example.com>
2024-09-29 15:48:11 +0300

    l2

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,21 +1,5 @@
-# Django Filter — это сторонняя библиотека, которая упрощает создание фильтров
-# для моделей Django.
-# django.contrib.auth относится к стандартным библиотекам фреймворка Django
-# recipes.models моя, импорты локальных приложений или библиотек
-# # Стандартная библиотека Django
-# from django.contrib.auth import get_user_model
-#
-# # Сторонняя библиотека
-# from django_filters import rest_framework as filters
-#
-# # Локальная библиотека
-# from recipes.models import Recipe
-# импорты разных категорий (стандартные библиотеки,
-#                          сторонние библиотеки,
-#                          локальные модули)
-# разделены пустыми строками, как и предписано PEP 8.
-from django.contrib.auth import get_user_model
 
+from django.contrib.auth import get_user_model
 from django_filters import rest_framework as filters
 
 from recipes.models import Recipe
